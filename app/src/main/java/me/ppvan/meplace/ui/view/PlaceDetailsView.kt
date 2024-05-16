5f96c41333f80869a7a18a48d4605c9c42d387b4
Silwee <21020093@vnu.edu.vn>
2024-05-16 16:50:14 +0700

    Update Rating trong PlaceDetailsView

--- app/src/main/java/me/ppvan/meplace/ui/view/PlaceDetailsView.kt
+++ app/src/main/java/me/ppvan/meplace/ui/view/PlaceDetailsView.kt
@@ -1,9 +1,6 @@
 package me.ppvan.meplace.ui.view
 
-import android.media.Rating
-import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,31 +11,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -46,31 +30,23 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import me.ppvan.meplace.MePlaceApplication
-import me.ppvan.meplace.Routes
-import me.ppvan.meplace.data.Schedule
 import me.ppvan.meplace.data.Destination
 import me.ppvan.meplace.data.Restaurant
 import me.ppvan.meplace.ui.component.BottomRoundedShape
 import me.ppvan.meplace.ui.component.CircleButton
-import me.ppvan.meplace.ui.component.ScheduleCard
 import me.ppvan.meplace.viewmodel.PlaceDetailsViewModel
 import me.ppvan.meplace.viewmodel.viewModelFactory
 import me.ppvan.meplace.ui.component.RestaurantCard
 
-import me.ppvan.meplace.viewmodel.RestaurantDetailsViewModel
 import me.ppvan.meplace.ui.component.UserRatingBar
 
 @Composable
@@ -138,7 +114,7 @@ fun PlaceDetailsView(id: Int, onBackPress: () -> Unit, navigateToDetail: (Int) -
 
         ResListRecommend(resList = viewModel.restaurants, modifier = Modifier, navigateToDetail)
 
-        UserRatingBar(modifier = Modifier.padding(horizontal = 24.dp, vertical = 20.dp), ratingState = currRate, rating = rating)
+        UserRatingBar(modifier = Modifier.padding(horizontal = 24.dp, vertical = 20.dp), ratingState = currRate, rating = rating, totalRating = destination.rate)
 
     }
 
